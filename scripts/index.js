e08853511599708c41a2f34a1379d60b57b728cb
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2021-12-08 09:11:52 +0100

    changed again since no libraries

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,38 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//Slå till 12. Det gäller att slå exakt 12 poäng på 3 slag.
+//Om ingen lyckas slå till 12 på 3 kast börjar spelet om igen.
+//Får fler 12 i samma omgång får de antingen slå om alla 3 slagen
+//eller tävla om vem som får högst poäng på ett slag.
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function moveCube(){
-    if (keys['ArrowLeft']){
-        cube.position.x -= 0.1;
+class Player {
+    constructor(id){
+        this.id = id;
+        this.score = 0;
+        this.rolls = [];
     }
-    if (keys['ArrowRight']){
-        cube.position.x += 0.1;
+    rollDice() {
+        this.rolls.push(Math.ceil(Math.random()*6));
+        if (this.rolls.length > 3) score = 0;
+        this.calcScore();
+    }
+
+    drawDiceRoll() {
+        const player = document.querySelector('player'+this.id);
+        const roll = document.createElement('p');
+        roll.textContent = 'roll ' + this.rolls.length + ': ' +this.rolls[this.rolls.length-1];
+        player.appendChild(roll);  
+    }
+
+    calcScore(){
+        this.rolls.forEach(roll => {
+            this.score += roll;
+        });
     }
 }
-dt = 0;
-function animate() {
-    moveCube();
-    renderer.render( scene, camera );
-	requestAnimationFrame(animate);
+
+let players = [];
+
+const createPlayer = () => {
+    players.push(new Player(players.length));
 }
 
-const plane = new THREE.Plane(new THREE.Vector3(1, 1, 1), 1);
-const raycaster = new THREE.Raycaster(); 
-const mouse = new THREE.Vector2();       
-const intersectPoint = new THREE.Vector3();
-document.addEventListener('mousemove', event =>{
-
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    raycaster.setFromCamera(mouse, camera);
-    raycaster.ray.intersectPlane(plane, intersectPoint);
-    cube.lookAt(intersectPoint);
-});
-
-let keys = [];
-
-document.addEventListener('keydown', x => {
-    keys[x.key] = true;
-});
-
-document.addEventListener('keyup', x => {
-    keys[x.key] = false;
-});
-
-animate();
+createPlayer();
